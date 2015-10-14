0a9cdabb713fcd947cef11a9cd397f53372b6fd5
Christian Paling <christian.paling@googlemail.com>
2015-10-14 22:51:27 +0200

    refactored startup manager test

--- test/test_startup_manager.rb
+++ test/test_startup_manager.rb
@@ -3,23 +3,23 @@ require 'fileutils'
 
 class TestStartupManager < Minitest::Test
   
-  def test_git_repository_should_be_false
-    startup = Peacock::StartupManager.new
-    Dir.mkdir 'temptest'
-    Dir.chdir 'temptest'
-    refute startup.git_repository?
-    Dir.chdir '..'
-    Dir.rmdir 'temptest'
+  def setup
+    @startup = Peacock::StartupManager.new
+    Dir.mkdir '/tmp/temptest'
+    Dir.chdir '/tmp/temptest'
   end
   
-  def test_git_repository_should_be_true
-    startup = Peacock::StartupManager.new
-    Dir.mkdir 'temptest'
-    Dir.chdir 'temptest'
-    Git.init
-    assert startup.git_repository?
-    Dir.chdir '..'
+  def teardown
+    Dir.chdir '/tmp'
     FileUtils.rm_rf 'temptest'
   end
   
+  def test_git_repository_should_be_false
+    refute @startup.git_repository?
+  end
+  
+  def test_git_repository_should_be_true
+    Git.init
+    assert @startup.git_repository?  
+  end
 end
