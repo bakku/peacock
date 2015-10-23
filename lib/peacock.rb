74274ed633bffccd0decea5df0a00a4d15be9c73
Christian Paling <christian.paling@googlemail.com>
2015-10-23 16:05:43 +0200

    changed workflow so you can view helppage without having to be in a git repo

--- lib/peacock.rb
+++ lib/peacock.rb
@@ -1,6 +1,6 @@
 require 'git'
 require 'peacock/version'
-require 'peacock/peacock_error'
+require 'peacock/error'
 require 'peacock/startup_manager'
 require 'peacock/cli_hash'
 require 'peacock/cli'
@@ -10,8 +10,8 @@ require 'peacock/ignorer'
 module Peacock
 
   def self.execute
-    Peacock::StartupManager.check_peacock_requirements
     cli_hash = Peacock::CLI.parse
+    Peacock::StartupManager.check_peacock_requirements
     Peacock::Ignorer.ignore(cli_hash)
   end
 
