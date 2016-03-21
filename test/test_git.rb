68aa03abd1c4601f782e02a18452b0f76842bff3
Christian Paling <christian.paling@googlemail.com>
2016-03-21 15:06:05 +0100

    updated git library tests

--- test/test_git.rb
+++ test/test_git.rb
@@ -1,30 +1,48 @@
 require 'minitest_helper'
 
 class TestGit < Minitest::Test
-  
+
   def setup
-    Dir.mkdir '/tmp/temptest'
-    Dir.chdir '/tmp/temptest'
+    Dir.mkdir '/tmp/peacock'
+    Dir.chdir '/tmp/peacock'
   end
-  
+
   def teardown
-    Dir.chdir '/tmp'
-    FileUtils.rm_rf 'temptest'
+    FileUtils.rm_rf '/tmp/peacock'
     ARGV.clear
   end
-  
+
   def test_git_new_should_raise_exception
     assert_raises NoGitRepositoryError do
       Git.check_repo_existance
     end
   end
-  
+
   def test_git_init_should_return_correct_output
     output = Git.init
     assert output.include?('Initialized empty Git repository')
     Dir.exists?('.git')
   end
-  
+
+  def test_git_status_should_return_correct_output
+    Git.init
+    output = Git.status
+    assert output.include?('Initial commit')
+    assert output.include?('nothing to commit')
+    FileUtils.touch 'file.txt'
+    Git.commit_all('initial commit')
+
+    output = Git.status
+    assert output.include?('On branch master')
+    assert output.include?('nothing to commit')
+
+    FileUtils.rm 'file.txt'
+    output = Git.status
+    assert output.include?('Changes not staged for commit')
+    assert output.include?('deleted')
+    assert output.include?('file.txt')
+  end
+
   def test_git_commit_all_should_return_correct_output
     Git.init
     FileUtils.touch 'file.txt'
@@ -33,5 +51,26 @@ class TestGit < Minitest::Test
     assert output.include?('commit')
     assert output.include?('test')
   end
-  
+
+  def test_git_remove_from_cache_should_work_as_expected
+    Git.init
+    FileUtils.touch 'file.txt'
+    Git.commit_all 'initial commit'
+    Git.remove_from_cache 'file.txt'
+    output = Git.status
+    assert output.include?('deleted')
+    assert output.include?('file.txt')
+  end
+
+  def test_git_clear_cache_should_work_as_expected
+    Git.init
+    FileUtils.touch 'file.txt'
+    FileUtils.touch 'file2.txt'
+    Git.commit_all 'initial commit'
+    Git.clear_cache
+    output = Git.status
+    assert output.include?('deleted')
+    assert output.include?('file.txt')
+    assert output.include?('file2.txt')
+  end
 end
