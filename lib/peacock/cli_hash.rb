84afe43124965ad9b28d963866845657a73c123a
Christian Paling <christian.paling@googlemail.com>
2016-03-25 18:10:56 +0100

    fixed --extract bug

--- lib/peacock/cli_hash.rb
+++ lib/peacock/cli_hash.rb
@@ -1,16 +1,16 @@
 module Peacock
 
   class CLIHash
-    
+
     attr_reader :hash
-    
+
     def initialize
       @hash = Hash.new
       @hash[:opts] = Array.new
       @hash[:files] = Array.new
       @hash[:dirs] = Array.new
     end
-    
+
     def push(type, str)
       if type == :dirs
         str = str + '/' unless str =~ /\/$/  # add backlash to dir name if it does not exist yet
@@ -19,39 +19,39 @@ module Peacock
 
       @hash[type].push(str)
     end
-    
+
     def opts
       @hash[:opts]
     end
-    
+
     def files
       @hash[:files]
     end
-    
+
     def dirs
       @hash[:dirs]
     end
-    
+
     def to_s
       hash.to_s
     end
-    
+
     def root_ignore?
       opts.include?('-r') || opts.include?('--root')
     end
-    
+
     def silent?
       opts.include?('-s') || opts.include?('--silent')
     end
-    
+
     def engine
-      if opts.include?('-e')
+      if opts.include?('-e') || opts.include?('--exclude')
         return Peacock::Engine::Extractor
       else
         return Peacock::Engine::Ignorer
-      end  
+      end
     end
-    
+
   end
-  
+
 end
